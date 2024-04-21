28b2a5fd27f2bca19a008839ad5e39453ab1c1aa
Alexander Stepanov <alexanderlawyer97@gmail.com>
2024-04-21 19:35:47 +0400

    Initial Commit

---
+++ Project 007. Alamofire Table/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 007. Alamofire Table
+//
+//  Created by Mac Alexander on 21.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
